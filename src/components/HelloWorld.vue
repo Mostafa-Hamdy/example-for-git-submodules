648043218b7e6e1058d79f8f6e0b8374f13a62ff
Mostafa-Hamdi <MostafaElMarengy11@gmail.com>
2024-06-03 14:45:40 +0300

    edit in hello world components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
+   this is hello world compoentns
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -24,6 +25,7 @@ const count = ref(0)
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
+
   </p>
   <p>
     Install
