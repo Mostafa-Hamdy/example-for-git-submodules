c741343719e02fb1843aca7ce7c12a659ecf7c45
Mostafa-Hamdi <MostafaElMarengy11@gmail.com>
2024-06-03 14:35:47 +0300

    this is edit in submodule

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      this is sub-repo
+   </div>
+</template>
+
+<script>
+export default {
+   setup () {
+      
+
+      return {}
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
